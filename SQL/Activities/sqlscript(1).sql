b4ec982b91256d9ba3d6eb2aaae40fc309b5c79c
DhanshreeManeibm <136333171+DhanshreeManeibm@users.noreply.github.com>
2023-07-05 22:03:25 +0530

    Add files via upload

---
+++ SQL/Activities/sqlscript(1).sql
@@ -0,0 +1,21 @@
+REM   Script: ACTIVITY 1
+REM   SQL ACTIVITY 1 SOLUTION 
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+select * from salesman;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+select * from salesman;
+
---
+++ SQL/Activities/sqlscript(11).sql
@@ -0,0 +1,101 @@
+REM   Script: ACTIVITY 10
+REM   ACTIVITY 10 SOLUTION
+
+create table customers ( 
+    customer_id int primary key, customer_name varchar(32), 
+    city varchar(20), grade int, salesman_id int);
+
+INSERT ALL 
+    INTO customers VALUES (3002, 'Nick Rimando', 'New York', 100, 5001) 
+    INTO customers VALUES (3007, 'Brad Davis', 'New York', 200, 5001) 
+    INTO customers VALUES (3005, 'Graham Zusi', 'California', 200, 5002) 
+    INTO customers VALUES (3008, 'Julian Green', 'London', 300, 5002) 
+    INTO customers VALUES (3004, 'Fabian Johnson', 'Paris', 300, 5006) 
+    INTO customers VALUES (3009, 'Geoff Cameron', 'Berlin', 100, 5003) 
+    INTO customers VALUES (3003, 'Jozy Altidor', 'Moscow', 200, 5007) 
+    INTO customers VALUES (3001, 'Brad Guzan', 'London', 300, 5005) 
+SELECT 1 FROM DUAL;
+
+SELECT a.customer_name AS "Customer Name", a.city, b.name AS "Salesman", b.commission FROM customers a  
+INNER JOIN salesman b ON a.salesman_id=b.salesman_id;
+
+SELECT a.customer_name, a.city, a.grade, b.name AS "Salesman", b.city FROM customers a  
+LEFT OUTER JOIN salesman b ON a.salesman_id=b.salesman_id WHERE a.grade<300  
+ORDER BY a.customer_id;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+SELECT a.customer_name AS "Customer Name", a.city, b.name AS "Salesman", b.commission FROM customers a  
+INNER JOIN salesman b ON a.salesman_id=b.salesman_id;
+
+SELECT a.customer_name, a.city, a.grade, b.name AS "Salesman", b.city FROM customers a  
+LEFT OUTER JOIN salesman b ON a.salesman_id=b.salesman_id WHERE a.grade<300  
+ORDER BY a.customer_id;
+
+SELECT a.customer_name AS "Customer Name", a.city, b.name AS "Salesman", b.commission FROM customers a  
+INNER JOIN salesman b ON a.salesman_id=b.salesman_id;
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
+
+SELECT a.customer_name AS "Customer Name", a.city, b.name AS "Salesman", b.commission FROM customers a  
+INNER JOIN salesman b ON a.salesman_id=b.salesman_id;
+
+SELECT a.customer_name, a.city, a.grade, b.name AS "Salesman", b.city FROM customers a  
+LEFT OUTER JOIN salesman b ON a.salesman_id=b.salesman_id WHERE a.grade<300  
+ORDER BY a.customer_id;
+
+SELECT * FROM orders 
+WHERE salesman_id=(SELECT DISTINCT salesman_id FROM orders WHERE customer_id=3007);
+
+SELECT * FROM orders 
+WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE city='New York');
+
+SELECT grade, COUNT(*) FROM customers 
+GROUP BY grade HAVING grade>(SELECT AVG(grade) FROM customers WHERE city='New York');
+
+SELECT order_no, purchase_amount, order_date, salesman_id FROM orders 
+WHERE salesman_id IN( SELECT salesman_id FROM salesman 
+WHERE commission=( SELECT MAX(commission) FROM salesman));
+
+SELECT * FROM orders 
+WHERE salesman_id=(SELECT DISTINCT salesman_id FROM orders WHERE customer_id=3007);
+
+SELECT * FROM orders 
+WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE city='New York');
+
+SELECT grade, COUNT(*) FROM customers 
+GROUP BY grade HAVING grade>(SELECT AVG(grade) FROM customers WHERE city='New York');
+
+SELECT order_no, purchase_amount, order_date, salesman_id FROM orders 
+WHERE salesman_id IN( SELECT salesman_id FROM salesman 
+WHERE commission=( SELECT MAX(commission) FROM salesman));
+
